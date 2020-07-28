c237a20f75151d1cf56a65b94c6e2c77d0badc92
regeshengen <reges@unochapeco.edu.br>
2020-07-28 00:27:06 -0300

    navegação drawer

---
+++ navegacao/src/navegacao/Drawer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import TelaA from '../views/TelaA'
+import TelaB from '../views/TelaB'
+import TelaC from '../views/TelaC'
+
+const Drawer = new createDrawerNavigator()
+
+export default props => {
+    return(
+        <Drawer.Navigator initialRouteName="TelaB">
+
+            <Drawer.Screen name="TelaA" component={TelaA} />
+            <Drawer.Screen name="TelaB" component={TelaB} />
+            <Drawer.Screen name="TelaC" component={TelaC} />
+            
+        </Drawer.Navigator>
+    )
+}
--- navegacao/src/navegacao/index.js
+++ navegacao/src/navegacao/index.js
@@ -3,13 +3,15 @@ import { SafeAreaView, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Stack from './Stack'
 import Tab from './Tab'
+import Drawer from './Drawer'
 
 export default props => {
     return(
         <SafeAreaView style={{flex: 1}}>
             <NavigationContainer>
                 {/* <Stack></Stack> */}
-                <Tab></Tab>
+                {/* <Tab></Tab> */}
+                <Drawer />
             </NavigationContainer>
         </SafeAreaView>
     )
